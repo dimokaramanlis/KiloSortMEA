fed5e5e6a9614c66039b2c337379cb099f5b8a26
dimokaramanlis <dimokaramanlis@gmail.com>
2019-01-21 16:02:42 +0100

    Fixed magic number for spike centering

--- preProcess/get_PCproj.m
+++ preProcess/get_PCproj.m
@@ -1,11 +1,11 @@
 function Us = get_PCproj(S1, row, col, wPCA, maskMaxChans)
 
 [nT, nChan] = size(S1);
-dt = -21 + [1:size(wPCA,1)];
+%dt = -21 + [1:size(wPCA,1)];
+dt =-floor(21 * size(wPCA,1)/61)+ [1:size(wPCA,1)];
 inds = repmat(row', numel(dt), 1) + repmat(dt', 1, numel(row));
 
 clips = reshape(S1(inds, :), numel(dt), numel(row), nChan);
-
 
 mask = repmat([1:nChan], [numel(row) 1]) - repmat(col, 1, nChan);
 Mask(1,:,:) = abs(mask)<maskMaxChans;
